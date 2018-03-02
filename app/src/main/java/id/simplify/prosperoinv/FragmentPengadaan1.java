e005808aec0f8901d847c3dd83f8cdb314802d54
hikamhaikal <hikam.haikal2@gmail.com>
2018-03-03 04:47:27 +0700

    Second Push by Haikal

--- app/src/main/java/id/simplify/prosperoinv/FragmentPengadaan1.java
+++ app/src/main/java/id/simplify/prosperoinv/FragmentPengadaan1.java
@@ -2,6 +2,7 @@ package id.simplify.prosperoinv;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,4 +21,9 @@ public class FragmentPengadaan1 extends Fragment {
         return inflater.inflate(R.layout.tab_pengadaan1, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
