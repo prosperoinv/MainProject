37d38b417547f8425a5162d74e913904f9b2c8d8
widicahyani <widi.setiacahyani@gmail.com>
2018-03-16 19:02:44 +0700

    third push by widi

---
+++ app/src/main/java/id/simplify/prosperoinv/LaporanPengadaan.java
@@ -0,0 +1,13 @@
+package id.simplify.prosperoinv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaporanPengadaan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laporan_pengadaan);
+    }
+}
---
+++ app/src/main/java/id/simplify/prosperoinv/SplashScreen.java
@@ -0,0 +1,33 @@
+package id.simplify.prosperoinv;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int splashInterval = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent pertama = new Intent(SplashScreen.this, Login.class );
+                startActivity(pertama);
+                this.finish();
+            }
+
+            private void finish(){
+            } }, splashInterval);
+    };
+
+}
