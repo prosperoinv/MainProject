2d493060ebc75b4ab24cf3ddf1a26d51be4d5837
hikamhaikal <hikam.haikal2@gmail.com>
2018-03-23 01:05:56 +0700

    Merge branch 'master' of https://github.com/prosperoinv/MainProject
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/id/simplify/prosperoinv/SplashScreen.java
+++ app/src/main/java/id/simplify/prosperoinv/SplashScreen.java
@@ -14,10 +14,8 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -28,6 +26,6 @@ public class SplashScreen extends AppCompatActivity {
 
             private void finish(){
             } }, splashInterval);
-    };
+    }
 
 }
