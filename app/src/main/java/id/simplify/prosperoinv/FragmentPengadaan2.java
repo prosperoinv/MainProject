b636de9e00e737b8173d27c5a8c3d46b606e875a
widicahyani <widi.setiacahyani@gmail.com>
2018-03-24 08:54:03 +0700

    test

---
+++ app/src/main/java/id/simplify/prosperoinv/FragmentPengadaan2.java
@@ -0,0 +1,77 @@
+package id.simplify.prosperoinv;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListAdapter;
+
+import java.util.LinkedList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentPengadaan2 extends Fragment implements View.OnClickListener {
+    private final LinkedList<String> barang = new LinkedList<>();
+    private final LinkedList<String> vendor = new LinkedList<>();
+    private final LinkedList<String> jumlah = new LinkedList<>();
+
+    private RecyclerView mRecyclerView;
+    private PengadaanRec mAdapter;
+
+    public FragmentPengadaan2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.tab_pengadaan2, container, false);
+        insertData();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recviewer);
+        mAdapter = new PengadaanRec(getActivity(), barang, vendor, jumlah);
+        recyclerView.setAdapter(mAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+
+    }
+
+    private void insertData() {
+        for (int i = 0; i < 1; i++) {//pengulangan untuk menghasilkan data yang banyak
+            barang.add("Kulit"); //untuk menambah merk minuman
+            barang.add("Sol");
+            barang.add("Alas");
+            barang.add("Cat");
+            barang.add("Bahan");
+
+
+            vendor.add("A"); //untuk menambah deskripsi
+            vendor.add("B");
+            vendor.add("C");
+            vendor.add("D");
+            vendor.add("E");
+
+            jumlah.add("10"); //untuk menambahkan foto
+            jumlah.add("20");
+            jumlah.add("30");
+            jumlah.add("40");
+            jumlah.add("50");
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
---
+++ app/src/main/java/id/simplify/prosperoinv/Pengadaan3Activity.java
@@ -0,0 +1,73 @@
+package id.simplify.prosperoinv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class Pengadaan3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengadaan3);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Create an instance of the tab layout from the view.
+        // Membuat adanya tab layout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        // Set the text for each tab.
+        //Setting nama masing-masing tab
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label2));
+        // Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // Use PagerAdapter to manage page views in fragments.
+        // Each page is represented by its own fragment.
+        // This is another example of the adapter pattern.
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            //Apa yang terjadi ketika tab dipilih
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+    }
+    @Override
+    //Mengaplikasikan menu di atas (Laporan)
+    public boolean onCreateOptionsMenu(Menu menu_main) {
+        getMenuInflater().inflate(R.menu.main_menu, menu_main);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            //Berpindah laman
+            Intent intent = new Intent(this, LaporanPengadaan.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickOrder(View view) {
+    }
+}
