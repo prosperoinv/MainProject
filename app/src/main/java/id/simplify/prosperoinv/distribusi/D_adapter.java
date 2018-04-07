2e9e20c0d7486a7068454d0c3d80c8726b1e53e2
widicahyani <widi.setiacahyani@gmail.com>
2018-04-07 10:06:34 +0700

    distribusi by widi

---
+++ app/src/main/java/id/simplify/prosperoinv/distribusi/D_adapter.java
@@ -0,0 +1,15 @@
+package id.simplify.prosperoinv.distribusi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.simplify.prosperoinv.R;
+
+public class D_adapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_adapter);
+    }
+}
---
+++ app/src/main/java/id/simplify/prosperoinv/distribusi/Distribusi1.java
@@ -0,0 +1,15 @@
+package id.simplify.prosperoinv.distribusi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.simplify.prosperoinv.R;
+
+public class Distribusi1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distribusi1);
+    }
+}
