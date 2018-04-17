9c363a1e0378684bd80b090de7c1f0695d6c23ef
hikamhaikal <hikam.haikal2@gmail.com>
2018-04-17 21:36:30 +0700

    Alex: Sekian untuk push hari ini semoga besok bisa FirebaseUI database

--- app/src/main/java/id/simplify/prosperoinv/dummy.java
+++ app/src/main/java/id/simplify/prosperoinv/dummy.java
@@ -1,9 +1,18 @@
 package id.simplify.prosperoinv;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import id.simplify.prosperoinv.pengadaan.LaporanPengadaan;
+import id.simplify.prosperoinv.pengadaan.tbhbarangbaru;
 
 public class dummy extends AppCompatActivity {
     //Halo, linda su masuk
@@ -12,6 +21,8 @@ public class dummy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
     }
 
     public void gopengadaan(View view) {
@@ -34,5 +45,25 @@ public class dummy extends AppCompatActivity {
     public void gopenjualan(View view) {
             Intent e = new Intent(dummy.this,id.simplify.prosperoinv.penjualan.Penjualan1.class);
             startActivity(e);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu_main) {
+        getMenuInflater().inflate(R.menu.menu_dummy, menu_main);
+        return true;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            //Logout dan mencegah dapat mengklik back
+            FirebaseAuth.getInstance().signOut();
+            Intent a = new Intent(this, Login.class);
+            startActivity(a);
+            finishAffinity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
